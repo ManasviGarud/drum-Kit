6a9b5891c82b52e2b3ff008625fe1dd16d86232f
ManasviGarud <garudmanasvi26@gmail.com>
2025-07-10 12:47:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
+
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML=this.innerHTML;
-    switch (buttonInnerHTML) {
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML)
+  });
+
+}
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
       case "w":
         var crash  = new Audio("sounds/crash.mp3");
         crash.play();
@@ -45,5 +57,12 @@ for (var i = 0; i < numberOfDrums; i++) {
       default:console.log(buttonInnerHTML);
         break;
     }
-  });
 }
+function buttonAnimation(currentKey){
+ var activeButton= document.querySelector("."+currentKey);
+ activeButton.classList.add("pressed");
+ setTimeout(function(){
+  activeButton.classList.remove("pressed");
+ }, 100);
+}
+
